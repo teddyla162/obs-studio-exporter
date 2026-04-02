472624592779b4754c77b00e2b37173fcedf683a
Teddyla <daraapple7@gmail.com>
2026-04-03 05:07:19 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,11 +4,17 @@ import "C"
 import (
     "fmt"
     "net/http"
+    "os"
 )
 
 // This is called automatically by OBS when the plugin loads
 //export obs_module_load
 func obs_module_load() C.bool {
+    // Write to a log file so we can see if this runs
+    f, _ := os.Create("C:\\obs_plugin_debug.log")
+    f.WriteString("obs_module_load was called!\n")
+    f.Close()
+    
     go StartMetricsServer()
     return C.bool(true)
 }
@@ -36,20 +42,14 @@ func StopRecording() {
 //export StartMetricsServer
 func StartMetricsServer() {
     http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
-        // Get stats from OBS
         fmt.Fprintf(w, "# HELP obs_frames_total Total frames\n")
         fmt.Fprintf(w, "# TYPE obs_frames_total counter\n")
         fmt.Fprintf(w, "obs_frames_total 0\n")
         fmt.Fprintf(w, "# HELP obs_dropped_frames_total Dropped frames\n")
         fmt.Fprintf(w, "# TYPE obs_dropped_frames_total counter\n")
         fmt.Fprintf(w, "obs_dropped_frames_total 0\n")
-        fmt.Fprintf(w, "# HELP obs_bitrate_bytes_total Total bytes sent\n")
-        fmt.Fprintf(w, "# TYPE obs_bitrate_bytes_total counter\n")
-        fmt.Fprintf(w, "obs_bitrate_bytes_total 0\n")
     })
     http.ListenAndServe(":9407", nil)
 }
 
-func main() {
-    // Required for c-shared build mode
-}
+func main() {}
